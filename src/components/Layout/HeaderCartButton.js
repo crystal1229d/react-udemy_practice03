1903d2d519cd360df95197edda75b6697b942b2f
crystal1229d <crystal1229d@naver.com>
2022-06-04 03:05:41 +0900

    use css effect to HeaderCartButton 1) adapt useEffect & useState to apply css effect to cart button when item is added to cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,18 +1,40 @@
-import { useContext } from 'react'
+import { useContext, useEffect, useState } from 'react'
 
 import CartContext from '../../store/cart-context'
 import CartIcon from '../Cart/CartIcon'
 import classes from './HeaderCartButton.module.css'
 
 const HeaderCartButton = props => {
+  const [btnIsHightlighted, setBtnIsHightlighted] = useState(false)
   const cartContext = useContext(CartContext)
+  const { items } = cartContext
 
-  const numberOfCartItems = cartContext.items.reduce((currentNumber, item) => { 
+  const numberOfCartItems = items.reduce((currentNumber, item) => { 
     return currentNumber + item.amount;
   }, 0) 
 
+  const btnClasses = `${classes.button} ${btnIsHightlighted ? classes.bump : ''}`
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return
+    }
+    setBtnIsHightlighted(true)
+
+    const timer = setTimeout(() => {
+      setBtnIsHightlighted(false)
+    }, 300);
+
+    // cleanup func
+    return () => {
+      clearTimeout(timer)
+    }
+  
+  }, [items])
+  
+
   return (
-    <button className={classes.button} onClick={props.onClick}>
+    <button className={btnClasses} onClick={props.onClick}>
         <span className={classes.icon}>
             <CartIcon />
         </span>
